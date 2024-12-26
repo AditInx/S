da129549d23ea59de294208d5f581dcd78dff17d
aditshah <adit.s@inheritx.com>
2024-12-26 12:22:53 +0530

    made javascript part

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-let stop = document.getElementById('stop');
-let start = document.getElementById('start');
-let reset = document.getElementById('reset');
-let hours = document.getElementById('hours');
-let mins = document.getElementById('mins');
-let sec = document.getElementById('sec');
+let [seconds,minutes, hours] = [0,0,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+function stopWatch(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++;
+        }
+    }
 
-start.addEventListener('click',setInterval(() => {
-    start.innerHTML = start.innerHTML + 1000;
-}, 1000));
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+
+function watchStart(){
+    if(timer != null) {
+        clearInterval(timer);
+    }
+    timer = setInterval(stopWatch,1000);
+}
+
+function watchStop(){
+    clearInterval(timer);
+}
+
+function watchReset(){
+    clearInterval(timer);
+    [seconds,minutes,hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00";
+}
